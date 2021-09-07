d19cce9e95c3d4c74f6d8599ae3c1fcac275fb1c
Dark0zZz <francesco.novelli.dev@gmail.com>
2021-09-07 21:57:52 +0200

    longest return longest word

--- longest/index.js
+++ longest/index.js
@@ -4,10 +4,12 @@ console.log(longest(string));
 function longest(string) {
     let words = string.split(" ");
     let maxLength = 0;
+    let longestWord = "";
     words.forEach(word => {
         if (word.length > maxLength) {
             maxLength = word.length;
+            longestWord = word;
         }
     });
-    return maxLength;
+    return longestWord;
 }
