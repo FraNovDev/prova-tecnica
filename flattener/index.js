bd191d1cefdb7d7a99479a58fc049e0d8b82646e
Dark0zZz <francesco.novelli.dev@gmail.com>
2021-09-07 03:10:28 +0200

    use apply with arguments and this

--- flattener/index.js
+++ flattener/index.js
@@ -1,20 +1,28 @@
-let arr = [2, [3, [10, 11]], 1];
-var res = [];
-let num = 1;
-let t = a(arr);
-console.log(res);
+let arr = [2, [3, [10, 11]], 1, 't,'];
+console.log(flattener(arr));
+console.log(flatten(arr));
 
-function a(par) {
-    if (par instanceof Array) {
-        for (let index = 0; index < par.length; index++) {
-            let tmp = a(par[index]);
-            if (typeof tmp == 'number') {
-                res.push(tmp);
-            } else {
-                return res;
-            }
+function flattener(par) {
+    console.log(arguments[0]);
+    var res = [];
+    for (var i = 0; i < par.length; i++) {
+        if (par[i] instanceof Array) {
+            res.push.apply(res, flattener.apply(this, par[i]));
+        } else {
+            res.push(par[i]);
         }
-    } else {
-        return par;
     }
+    return res;
+}
+
+function flatten() {
+    var flat = [];
+    for (var i = 0; i < arguments.length; i++) {
+        if (arguments[i] instanceof Array) {
+            flat.push.apply(flat, flatten.apply(this, arguments[i]));
+        } else {
+            flat.push(arguments[i]);
+        }
+    }
+    return flat;
 }
