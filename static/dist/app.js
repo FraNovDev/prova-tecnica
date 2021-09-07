11c0afe8dd18110591efd259c25fdff76a116427
Dark0zZz <francesco.novelli.dev@gmail.com>
2021-09-08 00:54:12 +0200

    add application debug features

--- static/dist/app.js
+++ static/dist/app.js
@@ -1,13 +1,20 @@
 const http = require('http')
 const fs = require('fs')
 const url = require('url');
+const home = 'index.html';
 
 const server = http.createServer((req, res) => {
     var pathname = url.parse(req.url, true)
     var relReqPath = pathname.path.substring(1)
-    var stream = ((relReqPath.length == 0) ? 'index.html' : relReqPath);
+    var stream = ((relReqPath.length == 0) ? home : relReqPath);
     console.log("requested: " + stream)
-    fs.createReadStream(stream).pipe(res)
+    if (fs.existsSync(stream)) {
+        fs.createReadStream(stream).pipe(res)
+    } else {
+        console.log("---warning--- requested: '" + stream + "' does not exists! ---warning---")
+        fs.createReadStream(home).pipe(res)
+        res.writeHead(400);
+    }
 
 })
 server.listen(3000)
