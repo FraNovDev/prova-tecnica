baeada3d1e5031d631a7a47d9ade7f3638af5fa9
Francesco <francesco.novelli@wickedin.com>
2021-09-10 09:45:43 +0200

    use property length instead of variable

--- longest/index.js
+++ longest/index.js
@@ -3,11 +3,9 @@ console.log(longest(string));
 
 function longest(string) {
     var words = string.split(" ");
-    var maxLength = 0;
     var longestWord = "";
     words.forEach(word => {
-        if (word.length > maxLength) {
-            maxLength = word.length;
+        if (word.length > longestWord.length) {
             longestWord = word;
         }
     });
